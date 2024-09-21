6c37ace91453326c03f89d0c229549ade9ac2d43
~Humna <92547715+HumnaAnees@users.noreply.github.com>
2024-09-21 05:43:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function displayGreetings(){
+    let name = prompt("Enter your name");
+    let age = prompt("Enter your age");
+        if (age >= 18){
+            document.getElementById("displayGreetings")
+            document.getElementById("displayGreetings").innerHTML =` Welcome ${name} ,You are Eligible`;
+        }
+        else{
+            document.getElementById("displayGreetings")
+            document.getElementById("displayGreetings").innerHTML =`Sorry ${name} ,you are not Eligible`;
+     
+        }
+        document.get
+    }
+    displayGreetings();
